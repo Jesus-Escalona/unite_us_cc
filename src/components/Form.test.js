b67d9833e564c71cf524ae58da75ff2c99d91864
Jesus Escalona <jesusescalona@me.com>
2019-05-07 20:27:09 -0400

    adding more tests

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import Form from "./Form";
+import {useInputs} from '../custom_hooks/useInputs'
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure} from "enzyme";
+import { renderHook, act } from 'react-hooks-testing-library'
+import { shallow, configure, mount } from "enzyme";
 
 configure({ adapter: new Adapter() });
 
@@ -12,7 +14,26 @@ it('should render without problems', function () {
     ReactDOM.unmountComponentAtNode(div);
 });
 
+
+const form = mount(<Form/>);
+
 it('should have 4 inputs', function () {
-    const form = shallow(<Form/>);
     expect(form.find('input').length).toEqual(4);
 });
+
+it('should have inputs with className = input', function () {
+    form.find('input').forEach(node => {
+        expect(node.hasClass('input')).toEqual(true)
+    });
+});
+
+// it('should have 1st input with no className = error', function () {
+//     let input = form.find('input').at(0);
+//     const mockedEvent = { target: { value: "hi", name: "name"}, persist: () => null };
+//     act(() => {input.simulate('change', mockedEvent)});
+//     form.update();
+//     console.log(input.props());
+//     act(() => {input.simulate('blur', mockedEvent)});
+//     console.log(input.props());
+//     expect(input.hasClass('error')).toEqual(true)
+// });
